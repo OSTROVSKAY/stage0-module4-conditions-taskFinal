eaa6eeac754a62ec3c02f237ce6b09718f6053c9
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2023-11-23 22:20:35 +0300

    Finish : Module #4.1. Final Task

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -3,5 +3,109 @@ package school.mjc.stage0.conditions.finalTask;
 public class DaysInMonth {
     public void printDays(int year, int month) {
 
+        int day;
+
+        if( year > 0 &&  month > 0 &&  month < 13 ) {
+
+            if( (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0) ) {
+
+                // leap
+                day = 29;
+
+            } else {
+
+                // not leap
+                day = 28;
+
+            }
+
+            switch(month) {
+
+                default:
+
+                    System.out.println("wrong number!");
+
+                    break;
+
+                case 1:
+
+                    System.out.println(31);
+
+                    break;
+
+                case 2:
+
+                    System.out.println(day);
+
+                    break;
+
+                case 3:
+
+                    System.out.println(31);
+
+                    break;
+
+                case 4:
+
+                    System.out.println(30);
+
+                    break;
+
+                case 5:
+
+                    System.out.println(31);
+
+                    break;
+
+                case 6:
+
+                    System.out.println(30);
+
+                    break;
+
+                case 7:
+
+                    System.out.println(31);
+
+                    break;
+
+                case 8:
+
+                    System.out.println(31);
+
+                    break;
+
+                case 9:
+
+                    System.out.println(30);
+
+                    break;
+
+                case 10:
+
+                    System.out.println(31);
+
+                    break;
+
+                case 11:
+
+                    System.out.println(30);
+
+                    break;
+
+                case 12:
+
+                    System.out.println(31);
+
+                    break;
+
+            }
+
+        } else {
+
+            System.out.println("invalid date");
+
+        }
+
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -3,5 +3,17 @@ package school.mjc.stage0.conditions.finalTask;
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
 
+        boolean check = firstSide > 0 && secondSide > 0 && thirdSide > 0 && (firstSide < secondSide + thirdSide) && (secondSide < firstSide + thirdSide) && (thirdSide < firstSide + secondSide);
+
+        if(check) {
+
+            System.out.println("this is a valid triangle");
+
+        } else {
+
+          System.out.println("it's not a triangle");
+
+        }
+
     }
 }
